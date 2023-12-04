b33df320e5c1206e6b208ba310ceb12f00ecdde4
KaengEE <yopuiqwer@naver.com>
2023-12-04 10:02:40 +0900

    가입페이지 추가

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,5 +1,47 @@
+import { useState } from "react";
 import styles from "./Signup.module.css";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, name);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles["signup-form"]}>
+      <h2>Sign up</h2>
+      <label>
+        {/* 이메일 */}
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        {/* 패스워드 */}
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        {/* 이름 */}
+        <span>name:</span>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+      </label>
+      <button className="btn">가입하기</button>
+    </form>
+  );
 }
