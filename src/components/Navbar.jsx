9db157a1ee5e3619d6d1c9c650f240270389be0a
KaengEE <yopuiqwer@naver.com>
2023-12-04 11:53:43 +0900

    로그아웃 버튼 추가

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 //CSS모듈사용시 CSS클래스가 다른 컴포넌트에도 적용되는 것을 방지
 import styles from "./Navbar.module.css";
+import { useLogout } from "../hooks/userLogout";
 
 export default function Navbar() {
+  const { logout } = useLogout();
+
   return (
     <nav className={styles.navbar}>
       <ul>
@@ -13,6 +16,12 @@ export default function Navbar() {
         </li>
         <li>
           <Link to="/signup">가입</Link>
+        </li>
+        {/* 로그아웃 버튼 */}
+        <li>
+          <button className="btn" onClick={logout}>
+            로그아웃
+          </button>
         </li>
       </ul>
     </nav>
