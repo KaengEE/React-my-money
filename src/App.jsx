1e596508f00ace4e69f4dd9717ac7717ed0bb986
KaengEE <yopuiqwer@naver.com>
2023-12-04 09:28:14 +0900

    페이지에 라우트 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
 function App() {
-  return <div className="App">마이머니앱</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
