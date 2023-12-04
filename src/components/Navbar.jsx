9ccf6d1633885a4391ef8a780a5ae9b817d45b6b
KaengEE <yopuiqwer@naver.com>
2023-12-04 09:39:47 +0900

    네브바 추가 및 CSS모듈 적용

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+//CSS모듈사용시 CSS클래스가 다른 컴포넌트에도 적용되는 것을 방지
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>My Money</li>
+
+        <li>
+          <Link to="/login">로그인</Link>
+        </li>
+        <li>
+          <Link to="/signup">가입</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
