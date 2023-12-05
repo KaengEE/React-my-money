19d12b1777089fed53c1fe22d2c789085f78e128
KaengEE <yopuiqwer@naver.com>
2023-12-05 10:01:39 +0900

    유저가 작성한 거래만 출력하기

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
 import { firedb } from "../firebase/config";
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, query) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     let ref = firedb.collection(collection);
+
+    //쿼리가 있으면 배열을 값으로 변환 [1,2,3] => (1,2,3)
+    if (query) {
+      ref = ref.where(...query);
+    }
 
     const unsub = ref.onSnapshot(
       (snapshot) => {
