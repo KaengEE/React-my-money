05cb7c8689329236a54980806b9836f3526d43c7
KaengEE <yopuiqwer@naver.com>
2023-12-05 12:31:41 +0900

    select박스 css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,26 +7,33 @@ import TransactionList from "./TransactionList";
 
 export default function Home() {
   const { user } = useAuthContext();
-  const [sortBy, setSortBy] = useState("createdAt"); // 기본값
+  const [sortBy, setSortBy] = useState({ by: "createdAt", order: "desc" }); // 기본값
   const { documents, error } = useCollection(
     "transactions",
     ["uid", "==", user.uid],
-    [sortBy, "desc"]
+    [sortBy.by, sortBy.order]
   );
 
   const handleSortChange = (e) => {
-    setSortBy(e.target.value);
+    const { name, value } = e.target;
+    setSortBy((prev) => ({ ...prev, [name]: value }));
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        {/* 정렬 */}
-        <select onChange={handleSortChange} value={sortBy}>
-          <option value="createdAt">최신순</option>
-          <option value="amount">비용순</option>
-          <option value="name">가나다순</option>
-        </select>
+        <div className={styles.selectbox}>
+          {/* 정렬 */}
+          <select name="by" onChange={handleSortChange} value={sortBy.by}>
+            <option value="createdAt">최신순</option>
+            <option value="amount">비용순</option>
+            <option value="name">가나다순</option>
+          </select>
+          <select name="order" onChange={handleSortChange} value={sortBy.order}>
+            <option value="asc">오름차순</option>
+            <option value="desc">내림차순</option>
+          </select>
+        </div>
         {error && <p>{error}</p>}
         {documents && <TransactionList transactions={documents} />}
       </div>
