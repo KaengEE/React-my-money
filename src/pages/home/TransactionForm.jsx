d82765054ab76d805ebe141ca40df4bf55fd97fb
KaengEE <yopuiqwer@naver.com>
2023-12-05 09:22:41 +0900

    firebase에 데이터 저장(addDocument 사용)

--- src/pages/home/TransactionForm.jsx
+++ src/pages/home/TransactionForm.jsx
@@ -1,16 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useFirestore } from "../../hooks/useFirestore";
 
-export default function TransactionForm() {
+export default function TransactionForm({ uid }) {
   const [name, setName] = useState("");
   const [amount, setAmount] = useState("");
+  const { addDocument, response } = useFirestore("transactions"); //DB컬렉션 이름 transactions
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({
-      name,
-      amount,
-    });
+    //console.log({name, amount});
+    addDocument({ uid, name, amount });
   };
+
+  useEffect(() => {
+    if (response.success) {
+      //저장성공시 입력창 빈칸
+      setName("");
+      setAmount("");
+    }
+  }, [response.success]);
+
   return (
     <div>
       <h3>거래 추가</h3>
