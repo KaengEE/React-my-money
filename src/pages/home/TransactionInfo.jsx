c89b4a813f58783f3b25d17ba1d5cc393c30f21b
KaengEE <yopuiqwer@naver.com>
2023-12-05 20:36:12 +0900

    총합,개수 uid에 따라 적용되게 수정

--- src/pages/home/TransactionInfo.jsx
+++ src/pages/home/TransactionInfo.jsx
@@ -1,22 +1,26 @@
 import styles from "./TransactionInfo.module.css";
 import { useCollection } from "../../hooks/useCollection";
 
-export const TransactionInfo = () => {
-  const { total, count } = useCollection("transactions");
+export const TransactionInfo = ({ uid }) => {
+  const { total, count } = useCollection("transactions", ["uid", "==", uid]);
   return (
     <div>
       <form>
         <label>
           <span>총 거래수</span>
           <input
-            readOnly="true"
+            readOnly={true}
             value={`${count} 건`}
             className={styles.input}
           />
         </label>
         <label>
           <span>총합계</span>
-          <input readOnly="true" value={`${total} 원`} />
+          <input
+            readOnly={true}
+            value={`${total} 원`}
+            className={styles.input}
+          />
         </label>
       </form>
     </div>
